26c142c73e1b504b276c3af56b287082ad0490ef
Marcus Fihlon <marcus@fihlon.ch>
2019-05-06 14:01:00 +0200

    :bookmark: Bumping version to 1.4-SNAPSHOT

--- src/main/java/sk/freemap/gpxAnimator/Constants.java
+++ src/main/java/sk/freemap/gpxAnimator/Constants.java
@@ -3,8 +3,8 @@ package sk.freemap.gpxAnimator;
 public enum Constants {
 	;
 
-	public static String VERSION = "1.3.1";
+	public static String VERSION = "1.4-SNAPSHOT";
 
-	public static String YEAR = "2018";
+	public static String YEAR = "2019";
 
 }
