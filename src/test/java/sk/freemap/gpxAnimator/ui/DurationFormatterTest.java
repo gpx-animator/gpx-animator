8951b61a484f89f485f4a2ed57e6427d258470fa
heroichornet <schnipper@gmail.com>
2019-11-11 22:05:32 +0100

    :recycle: Re-factored test DurationFormatterTest.java to be parametrized

--- src/test/java/sk/freemap/gpxAnimator/ui/DurationFormatterTest.java
+++ src/test/java/sk/freemap/gpxAnimator/ui/DurationFormatterTest.java
@@ -1,50 +1,42 @@
 package sk.freemap.gpxAnimator.ui;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.text.ParseException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DurationFormatterTest {
 
-    private static DurationFormatter df;
-    private static Map<Long,String> edgeCases = new HashMap<>();
-
-    @BeforeAll
-    static void Setup(){
-
-        df = new DurationFormatter();
-        edgeCases.put(Long.MIN_VALUE,"-106751991167d -7h -12m -55s -808ms");
-        edgeCases.put(Long.MAX_VALUE,"106751991167d 7h 12m 55s 807ms");
-        edgeCases.put(-1L,"0d 0h 0m 0s -1ms");
-        edgeCases.put(0L,"0d 0h 0m 0s 0ms");
-        edgeCases.put(100L,"0d 0h 0m 0s 100ms");
-        edgeCases.put(-100L,"0d 0h 0m 0s -100ms");
-        edgeCases.put(null,"");
-
+    private static Stream<Arguments> generateGetterSetterTestData() {
+        return Stream.of(
+                Arguments.of(Long.MIN_VALUE,"-106751991167d -7h -12m -55s -808ms"),
+                Arguments.of(Long.MAX_VALUE,"106751991167d 7h 12m 55s 807ms"),
+                Arguments.of(-1L,"0d 0h 0m 0s -1ms"),
+                Arguments.of(0L,"0d 0h 0m 0s 0ms"),
+                Arguments.of(100L,"0d 0h 0m 0s 100ms"),
+                Arguments.of(-100L,"0d 0h 0m 0s -100ms"),
+                Arguments.of(null,""));
     }
 
-    @Test
-    void stringToValue(){
-
-        for (Map.Entry<Long, String> edgeCase : edgeCases.entrySet()) {
-            try {
-                assertEquals(edgeCase.getKey(),df.stringToValue(edgeCase.getValue()));
-            }catch(ParseException e){
-                assert (false);
-            }
+    @ParameterizedTest
+    @MethodSource("generateGetterSetterTestData")
+    void stringToValue(final Long timestamp,final String timeText){
+        final DurationFormatter testee = new DurationFormatter();
+        try {
+            assertEquals(timestamp,testee.stringToValue(timeText));
+        }catch(ParseException e){
+            assert (false);
         }
-
     }
 
-    @Test
-    void valueToString() throws ParseException {
-        for (Map.Entry<Long, String> edgeCase : edgeCases.entrySet()) {
-            assertEquals(edgeCase.getValue(), df.valueToString(edgeCase.getKey()));
-        }
+    @ParameterizedTest
+    @MethodSource("generateGetterSetterTestData")
+    void valueToString(final Long timestamp,final String timeText) throws ParseException {
+        final DurationFormatter testee = new DurationFormatter();
+        assertEquals(timeText, testee.valueToString(timestamp));
     }
 }
