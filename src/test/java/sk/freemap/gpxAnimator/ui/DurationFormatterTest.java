3ffefda8e21bf7b62d935dca9bfc627947fde1b6
Marcus Fihlon <marcus@fihlon.swiss>
2019-11-11 22:52:43 +0100

    :ok_hand: Correcting code format

--- src/test/java/sk/freemap/gpxAnimator/ui/DurationFormatterTest.java
+++ src/test/java/sk/freemap/gpxAnimator/ui/DurationFormatterTest.java
@@ -13,29 +13,29 @@ class DurationFormatterTest {
 
     private static Stream<Arguments> generateTestData() {
         return Stream.of(
-                Arguments.of(Long.MIN_VALUE,"-106751991167d -7h -12m -55s -808ms"),
-                Arguments.of(Long.MAX_VALUE,"106751991167d 7h 12m 55s 807ms"),
-                Arguments.of(-1L,"0d 0h 0m 0s -1ms"),
-                Arguments.of(0L,"0d 0h 0m 0s 0ms"),
-                Arguments.of(100L,"0d 0h 0m 0s 100ms"),
-                Arguments.of(-100L,"0d 0h 0m 0s -100ms"),
-                Arguments.of(null,""));
+            Arguments.of(Long.MIN_VALUE, "-106751991167d -7h -12m -55s -808ms"),
+            Arguments.of(Long.MAX_VALUE, "106751991167d 7h 12m 55s 807ms"),
+            Arguments.of(-1L, "0d 0h 0m 0s -1ms"),
+            Arguments.of(0L, "0d 0h 0m 0s 0ms"),
+            Arguments.of(100L, "0d 0h 0m 0s 100ms"),
+            Arguments.of(-100L, "0d 0h 0m 0s -100ms"),
+            Arguments.of(null, ""));
     }
 
     @ParameterizedTest
     @MethodSource("generateTestData")
-    void stringToValue(final Long timestamp,final String timeText){
+    void stringToValue(final Long timestamp, final String timeText) {
         final DurationFormatter testee = new DurationFormatter();
         try {
-            assertEquals(timestamp,testee.stringToValue(timeText));
-        }catch(ParseException e){
+            assertEquals(timestamp, testee.stringToValue(timeText));
+        } catch (ParseException e) {
             assert (false);
         }
     }
 
     @ParameterizedTest
     @MethodSource("generateTestData")
-    void valueToString(final Long timestamp,final String timeText) throws ParseException {
+    void valueToString(final Long timestamp, final String timeText) throws ParseException {
         final DurationFormatter testee = new DurationFormatter();
         assertEquals(timeText, testee.valueToString(timestamp));
     }
