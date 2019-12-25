795c60ddbac2152ff8b1bd9298fa3eb0b4b6cc02
Marcus Fihlon <marcus@fihlon.swiss>
2019-12-25 11:34:43 +0100

    :ok_hand: Method ignores results of InputStream.read() #119
     This method ignores the return value of one of the variants of java.io.InputStream.read() which can return multiple bytes.  If the return value is not checked, the caller will not be able to correctly handle the case where fewer bytes were read than the caller requested.  This is a particularly insidious kind of bug, because in many programs, reads from input streams usually do read the full amount of data requested, causing the program to fail only sporadically.

--- src/main/java/sk/freemap/gpxAnimator/GpxParser.java
+++ src/main/java/sk/freemap/gpxAnimator/GpxParser.java
@@ -62,8 +62,8 @@ class GpxParser {
     public static InputStream decompressStream(final InputStream input) throws IOException {
         final PushbackInputStream pb = new PushbackInputStream(input, 2);
         final byte[] signature = new byte[2];
-        pb.read(signature);
-        pb.unread(signature);
+        final int numBytesRead = pb.read(signature);
+        pb.unread(signature, 0, numBytesRead);
         return signature[0] == (byte) 0x1f && signature[1] == (byte) 0x8b ? new GZIPInputStream(pb) : pb;
     }
 
