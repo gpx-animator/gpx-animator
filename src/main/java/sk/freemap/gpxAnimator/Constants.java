91c594239ed0e976cc73ae510912329724621a09
Marcus Fihlon <marcus@fihlon.swiss>
2020-01-08 22:49:46 +0100

    :globe_with_meridians: Localizing Constants #33

--- src/main/java/sk/freemap/gpxAnimator/Constants.java
+++ src/main/java/sk/freemap/gpxAnimator/Constants.java
@@ -4,9 +4,9 @@ package sk.freemap.gpxAnimator;
 public enum Constants {
     ;
 
-    public static final String APPNAME = "GPX Animator";
+    public static final String APPNAME = "GPX Animator"; //NON-NLS
 
-    public static final String VERSION = "1.5-SNAPSHOT";
+    public static final String VERSION = "1.5-SNAPSHOT"; //NON-NLS
 
     public static final String YEAR = "2020";
 
