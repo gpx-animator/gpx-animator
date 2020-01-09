f04dee7fd4f755329ff765e38bf9a6677ad00c4e
Marcus Fihlon <marcus@fihlon.swiss>
2020-01-09 09:29:01 +0100

    :globe_with_meridians: Localizing Main #33

--- src/main/java/sk/freemap/gpxAnimator/Main.java
+++ src/main/java/sk/freemap/gpxAnimator/Main.java
@@ -20,6 +20,7 @@ import sk.freemap.gpxAnimator.ui.MainFrame;
 
 import java.awt.EventQueue;
 import java.awt.GraphicsEnvironment;
+import java.util.ResourceBundle;
 
 
 public final class Main {
@@ -27,10 +28,12 @@ public final class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     private Main() throws InstantiationException {
-        throw new InstantiationException("Utility classes can't be instantiated!");
+        throw new InstantiationException("This is the main class and can't be instantiated!");
     }
 
     public static void main(final String[] args) {
+        final ResourceBundle resourceBundle = Preferences.getResourceBundle();
+
         try {
             final CommandLineConfigurationFactory cf = new CommandLineConfigurationFactory(args);
             final Configuration configuration = cf.getConfiguration();
@@ -61,7 +64,7 @@ public final class Main {
                 });
             }
         } catch (final UserException e) {
-            LOGGER.error("Very bad, exception caught in main method!", e);
+            LOGGER.error(resourceBundle.getString("main.exception.log"), e);
             System.exit(1); // NOPMD -- We can't recover here
         }
     }
