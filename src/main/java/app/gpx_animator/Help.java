d0bab948927c69bb49137229f119831561d9665e
Marcus Fihlon <marcus@fihlon.swiss>
2020-12-31 20:02:51 +0100

    :pencil2: Fixing typo

--- src/main/java/app/gpx_animator/Help.java
+++ src/main/java/app/gpx_animator/Help.java
@@ -71,7 +71,7 @@ public final class Help {
         w.writeOptionHelp(Option.TIME_OFFSET, "milliseconds", true, tc.getTimeOffset()); //NON-NLS
         w.writeOptionHelp(Option.TMS_URL_TEMPLATE, "template", false, cfg.getTmsUrlTemplate()); //NON-NLS
         w.writeOptionHelp(Option.TOTAL_TIME, "time", false, cfg.getTotalTime()); //NON-NLS
-        w.writeOptionHelp(Option.SPEED_UNIT, "SPEED", false, cfg.getSpeedUnit()); //NON-NLS
+        w.writeOptionHelp(Option.SPEED_UNIT, "speed", false, cfg.getSpeedUnit()); //NON-NLS
         w.writeOptionHelp(Option.VIEWPORT_WIDTH, "viewport-width", false, cfg.getViewPortWidth()); //NON-NLS
         w.writeOptionHelp(Option.VIEWPORT_HEIGHT, "viewport-height", false, cfg.getViewPortHeight()); //NON-NLS
         w.writeOptionHelp(Option.VIEWPORT_INERTIA, "viewport-inertia", false, cfg.getViewPortInertia()); //NON-NLS
