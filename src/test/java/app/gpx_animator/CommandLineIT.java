fc4fb62172ca27a898a25fa3a68613aa336826b0
Marcus Fihlon <marcus@fihlon.swiss>
2021-01-03 22:11:49 +0100

    :white_check_mark: Fixing test on macOS (file will be bigger when generated on macOS)

--- src/test/java/app/gpx_animator/CommandLineIT.java
+++ src/test/java/app/gpx_animator/CommandLineIT.java
@@ -61,7 +61,7 @@ public final class CommandLineIT {
 
         final var fileSize = new File(outputFile).length();
         assertTrue(fileSize > 550_000, "Output file size (%s bytes) too small, check content".formatted(fileSize));
-        assertTrue(fileSize < 600_000, "Output file size (%s bytes) too big, check content".formatted(fileSize));
+        assertTrue(fileSize < 650_000, "Output file size (%s bytes) too big, check content".formatted(fileSize));
     }
 
     @AfterEach
