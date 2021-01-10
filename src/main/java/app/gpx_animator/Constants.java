eceadfefa581c15857429e5b71adf1a5d6e12d98
Marcus Fihlon <marcus@fihlon.swiss>
2021-01-10 20:08:30 +0100

    :bookmark: Pushing version number to 1.7.0-SNAPSHOT for the ongoing work on the next release of GPX Animator.

--- src/main/java/app/gpx_animator/Constants.java
+++ src/main/java/app/gpx_animator/Constants.java
@@ -8,7 +8,7 @@ public enum Constants {
 
     public static final String APPNAME = "GPX Animator"; //NON-NLS
 
-    public static final String VERSION = "1.6.0"; //NON-NLS
+    public static final String VERSION = "1.7.0-SNAPSHOT"; //NON-NLS
 
     public static final String APPNAME_VERSION = APPNAME.concat(" ").concat(VERSION);
 
