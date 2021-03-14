72678febfc93490ce8aecbba7d9c6a368b9c8673
Marcus Fihlon <marcus@fihlon.swiss>
2021-03-14 10:20:01 +0100

    :recycle: Removing hard coded version string and using value from build file instead.

--- src/main/java/app/gpx_animator/core/Constants.java
+++ src/main/java/app/gpx_animator/core/Constants.java
@@ -1,6 +1,8 @@
 package app.gpx_animator.core;
 
 import java.awt.Font;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 @SuppressWarnings("checkstyle:NoWhitespaceBefore") // For the ";" after the class declaration which is needed to use an enum for constants only
 public enum Constants {
@@ -8,7 +10,7 @@ public enum Constants {
 
     public static final String APPNAME = "GPX Animator"; //NON-NLS
 
-    public static final String VERSION = "1.7.0-SNAPSHOT"; //NON-NLS
+    public static final String VERSION = loadVersionString();
 
     public static final String APPNAME_VERSION = APPNAME.concat(" ").concat(VERSION);
 
@@ -23,4 +25,14 @@ public enum Constants {
     public static final String OS_ARCH = System.getProperty("os.arch");
 
     public static final Font DEFAULT_FONT = new Font(Font.MONOSPACED, Font.PLAIN, 12);
+
+    private static String loadVersionString() {
+        final var stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("version.txt");
+        assert stream != null : "Version file is missing!";
+        try (Scanner scanner = new Scanner(stream, StandardCharsets.UTF_8)) {
+            return scanner.nextLine();
+        } catch (final Exception e) {
+            return "<UNKNOWN VERSION>";
+        }
+    }
 }
