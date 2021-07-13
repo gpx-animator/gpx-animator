7d2726b63eae48f3d1d04575e5fc9d42293831c6
Marcus Fihlon <marcus@fihlon.swiss>
2021-07-13 21:41:31 +0200

    :rotating_light: Remove temporary suppression after Spotbugs update

--- src/main/java/app/gpx_animator/core/renderer/Metadata.java
+++ src/main/java/app/gpx_animator/core/renderer/Metadata.java
@@ -15,9 +15,7 @@
  */
 package app.gpx_animator.core.renderer;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.jetbrains.annotations.Nullable;
 
-@SuppressFBWarnings("EQ_UNUSUAL") // TODO SpotBugs 4.2.0 does not know how to handle the equals method of Java records #305
 public record Metadata(@Nullable Integer zoom, double minX, double maxX, double minY, double maxY, long minTime, long maxTime,
                        double speedup, int frames) { }
