7b82f4b220fdd1beab4fe02a16e7d9979b0d293f
Marcus Fihlon <marcus@fihlon.swiss>
2021-07-13 21:32:22 +0200

    :white_check_mark: Update assertions because the video is generated without a background map
    The test was using the standard background map before. Now the standard is no map. That is much better for the test, too, because otherwise the test needed working internet to succeed.

--- src/test/java/app/gpx_animator/ui/cli/CommandLineIT.java
+++ src/test/java/app/gpx_animator/ui/cli/CommandLineIT.java
@@ -63,8 +63,8 @@ public final class CommandLineIT {
         assertDone();
 
         final var fileSize = new File(outputFile).length();
-        assertTrue(fileSize > 500_000, "Output file size (%s bytes) too small, check content".formatted(fileSize));
-        assertTrue(fileSize < 600_000, "Output file size (%s bytes) too big, check content".formatted(fileSize));
+        assertTrue(fileSize > 300_000, "Output file size (%s bytes) too small, check content".formatted(fileSize));
+        assertTrue(fileSize < 350_000, "Output file size (%s bytes) too big, check content".formatted(fileSize));
     }
 
     @AfterEach
