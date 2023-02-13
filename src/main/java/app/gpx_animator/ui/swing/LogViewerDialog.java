ed32b1597217491463411a88e9e2d60efc2644d6
Hendrik Schick <hendrik.schick@adcubum.com>
2023-02-13 23:50:24 +0100

    buttons to copy log

--- src/main/java/app/gpx_animator/ui/swing/LogViewerDialog.java
+++ src/main/java/app/gpx_animator/ui/swing/LogViewerDialog.java
@@ -9,11 +9,12 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.io.Serial;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class LogViewerDialog extends EscapeDialog {
 
@@ -35,27 +36,47 @@ public class LogViewerDialog extends EscapeDialog {
         contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
         getContentPane().add(contentPanel, BorderLayout.CENTER);
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.LINE_AXIS));
-        var lines = readLogFile();
+
         var textArea = new JTextArea();
+        textArea.setEditable(false);
         var font = new Font(textArea.getFont().getName(), textArea.getFont().getStyle(), TEXT_SIZE);
         textArea.setFont(font);
 
-        var log = lines.isBlank() ? "" : lines;
+        var lines = readLogFile();
+        var log = lines.isEmpty() ? "" : String.join("\n", lines);
         textArea.setText(log);
 
         final var scrollPlane = new JScrollPane(textArea);
         contentPanel.add(scrollPlane);
+
+        final var buttonPanel = new JPanel();
+        final var copyAllLogButton = new JButton("Copy all");
+        copyAllLogButton.addActionListener(e -> copyLog(lines));
+        buttonPanel.add(copyAllLogButton);
+        final var copy50LogButton = new JButton("Copy 50 lines");
+        copy50LogButton.addActionListener(e -> {
+            var copyLog = lines.stream().limit(50).toList();
+            copyLog(copyLog);
+        });
+        buttonPanel.add(copy50LogButton);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
 
-    private String readLogFile() {
+    private static void copyLog(List<String> log) {
+        final var copyLog = String.join("\n", log);
+        final var selection = new StringSelection(copyLog);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, null);
+    }
+
+    private List<String> readLogFile() {
         var loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         var logbackLogger = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME);
         var fileAppender = (FileAppender<?>) logbackLogger.getAppender("FILE");
         var file = fileAppender.getFile();
         try (var lines = Files.lines(Path.of(file))){
-            return lines.collect(Collectors.joining("\n"));
+            return lines.toList();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Unable to open log file %s".formatted(file));
         }
     }
 }
