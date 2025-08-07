e9dd3195190efdcc2f8635672e4b0d83c51e55af
Marcus Fihlon <marcus@fihlon.swiss>
2025-08-07 11:10:28 +0200

    chore: fix linter warnings

--- src/main/java/app/gpx_animator/core/renderer/plugins/RendererPlugin.java
+++ src/main/java/app/gpx_animator/core/renderer/plugins/RendererPlugin.java
@@ -26,14 +26,14 @@ import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 
 /**
- * To extend the rendering process, just implement this interface. At the
+ * <p>To extend the rendering process, just implement this interface. At the
  * initialization of the rendering process, all classes implementing this
  * interface will be found and a constrcutor with the
  * <code>Configuration</code> as parameter will be called to create an
- * instance object.
+ * instance object.</p>
  *
- * Therefore, all implementations <em>must</em> provide a constructor with
- * the <code>Configuration</code> as the one and only parameter:
+ * <p>Therefore, all implementations <em>must</em> provide a constructor with
+ * the <code>Configuration</code> as the one and only parameter:</p>
  *
  * <pre>
  * {@code
@@ -44,11 +44,11 @@ import java.awt.image.BufferedImage;
 public interface RendererPlugin {
 
     /**
-     * This method returns an integer value which is used to call the plugins
-     * in a specified order to enable "layering" of the output.
+     * <p>This method returns an integer value which is used to call the plugins
+     * in a specified order to enable "layering" of the output.</p>
      *
-     * Plugins with a lower value are executed first, followed by plugins with
-     * a higher value. The default value is <code>0</code>
+     * <p>Plugins with a lower value are executed first, followed by plugins with
+     * a higher value. The default value is <code>0</code></p>
      *
      * @return the order number
      */
@@ -57,12 +57,12 @@ public interface RendererPlugin {
     }
 
     /**
-     * If your rendering plugin is adding more frames to the video, you have
+     * <p>If your rendering plugin is adding more frames to the video, you have
      * to return the number of added frames for the whole video before the
      * rendering starts. Do the calculation here and return the number of
-     * frames.
+     * frames.</p>
      *
-     * Default is to not add frames to the video.
+     * <p>Default is to not add frames to the video.</p>
      *
      * @return number of additional frames that will be added to the video
      */
@@ -71,42 +71,42 @@ public interface RendererPlugin {
     }
 
     /**
-     * Overwrite this method, if you want access to the metadata of the video.
+     * <p>Overwrite this method, if you want access to the metadata of the video.</p>
      *
      * @param metadata the metadata of the video
      */
-    default void setMetadata(@NonNull Metadata metadata) { }
+    default void setMetadata(final @NonNull Metadata metadata) { }
 
     /**
-     * Overwrite this method, if you want access to the frame writer for the
-     * video.
+     * <p>Overwrite this method, if you want access to the frame writer for the
+     * video.</p>
      *
      * @param frameWriter the frame writer for the video
      */
-    default void setFrameWriter(@NonNull FrameWriter frameWriter) { }
+    default void setFrameWriter(final @NonNull FrameWriter frameWriter) { }
 
     /**
-     * Overwrite this method, if you want access to the rendering context.
+     * <p>Overwrite this method, if you want access to the rendering context.</p>
      *
      * @param renderingContext the rendering context
      */
-    default void setRenderingContext(@NonNull RenderingContext renderingContext) { }
+    default void setRenderingContext(final @NonNull RenderingContext renderingContext) { }
 
     /**
-     * This render method is called to render the background image of the
+     * <p>This render method is called to render the background image of the
      * video. If the video does not use the moving map feature, the background
      * has the same size as the video. If the video makes use of the moving
      * map, the background has the size of the map behind the video, which
-     * usually is bigger than the viewport.
+     * usually is bigger than the viewport.</p>
      *
      * @param image   the background image to be modified
      * @throws UserException error to be shown to the user
      */
-    default void renderBackground(@NonNull BufferedImage image) throws UserException { }
+    default void renderBackground(final @NonNull BufferedImage image) throws UserException { }
 
     /**
-     * This render method is called to render one frame of the video. The size
-     * of this frame is equal to the resolution of the resulting video.
+     * <p>This render method is called to render one frame of the video. The size
+     * of this frame is equal to the resolution of the resulting video.</p>
      *
      * @param frame   the frame number
      * @param image   the image of the frame to be modified
@@ -114,10 +114,10 @@ public interface RendererPlugin {
      * @throws UserException error to be shown to the user
      */
     @SuppressWarnings("RedundantThrows") // implementations throw this exception
-    default void renderFrame(int frame, @Nullable Point2D marker, @NonNull BufferedImage image) throws UserException { }
+    default void renderFrame(final int frame, final @Nullable Point2D marker, final @NonNull BufferedImage image) throws UserException { }
 
     /**
-     * This method is called when the rendering has finished successfully.
+     * <p>This method is called when the rendering has finished successfully.</p>
      *
      * @throws UserException error to be shown to the user
      */
@@ -125,7 +125,7 @@ public interface RendererPlugin {
     default void renderingFinished() throws UserException { }
 
     /**
-     * This method is called when the rendering was canceled.
+     * <p>This method is called when the rendering was canceled.</p>
      *
      * @throws UserException error to be shown to the user
      */
